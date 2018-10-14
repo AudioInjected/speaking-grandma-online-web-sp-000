bf334667ab6d152458250979221bfbdd7e95335d
Rafael Batista <rafaelb24@yahoo.com>
2018-10-14 17:58:25 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,9 @@
 def speak_to_grandma(str)
   if str != str.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif str == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif str == str.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
